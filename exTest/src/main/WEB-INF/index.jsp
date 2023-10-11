28d63f328c0732e89ca9025fbdda0987572caeae
C.K <113984960+CK-0512@users.noreply.github.com>
2023-10-11 12:48:24 +0900

    boot study start

---
+++ exTest/src/main/WEB-INF/index.jsp
@@ -0,0 +1,13 @@
+<%@ page language="java" contentType="text/html; charset=EUC-KR"
+    pageEncoding="EUC-KR"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="EUC-KR">
+<title>Insert title here</title>
+</head>
+<body>
+	<h1>Spring JSP Web Test</h1>
+	<a href="#">1234</a>
+</body>
+</html>
---
+++ exTest/src/main/java/com/example/exTest/ExTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.exTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExTestApplication.class, args);
+	}
+
+}
